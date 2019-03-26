21eafe601d861aca6c5d1801f3bfc7268d05fe56
Johnliveira <jonhjonholiveira@gmail.com>
2019-03-26 17:42:31 -0300

    Altera e acrescenta os codigos para a reta final

--- scripts/previsao-tempo-controller.js
+++ scripts/previsao-tempo-controller.js
@@ -1,14 +1,12 @@
-angular.module('aplicacao').controller('PrevisaoTempoController', function($scope, $resouce) {
-	var WeatherChannel = $resouce('http://api.openweathermap.org/data/2.5/weather?q=SaoPaulo,BR&units=metric');
-	
-	var getWether = function() {
-		WeatherChannel.get().$promise.them(function(response) {
-			$scope.tempo = response;
-			console.log(response);
-		}, function(promise) {}
-			alert("Erro ao requisitar Recurso!");
-		})
-	}
-
-	getWeather();
-});
+angular.module('aplicacao').controller('PrevisaoTempoController', function ($scope, $resource) {
+    var weatherChannel = $resource('https://api.openweathermap.org/data/2.5/weather?id=3445831&units=metric&APPID=cb736063c590f2abe550a94e94645939');
+    var getWeather = function () {
+        weatherChannel.get().$promise.then(function (response) {
+            $scope.tempo = response;
+            console.log(response);
+        }, function () {
+            alert("Erro ao requisitar o clima!");
+        })
+    }
+    getWeather();
+})
